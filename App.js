7360c3755cdacc8cb44b94257c63f48077038afb
Eduardo SB <eduardosb95@gmail.com>
2020-04-13 20:40:44 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import {LoginButton, AccessToken} from 'react-native-fbsdk';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <LoginButton
+          onLoginFinished={(error, result) => {
+            if (error) {
+              console.log('login has error: ' + result.error);
+            } else if (result.isCancelled) {
+              console.log('login is cancelled.');
+            } else {
+              AccessToken.getCurrentAccessToken().then((data) => {
+                console.log(data.accessToken.toString());
+              });
+            }
+          }}
+          onLogoutFinished={() => console.log('logout.')}
+        />
+      </View>
+    );
+  }
+}
